76ff5f7ae5c7d7e9e517a9e479d077a29a3606b3
Lorenzo Martinico <lzmartinico@gmail.com>
2018-01-03 03:26:58 +0100

    Respond with JSON object

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Addition question generation #
 Service to generate an integer addition question within a certain range and provide four answers, three incorrect.
-Requests should be made to [http://localhost:8080/question](/question). Output will be in JSON format
+Requests should be made to [http://localhost:8080/question](/question) with URL parameters min and max being integers between 0 and 1,000,000 ([http://localhost:8080/question?min=4&max=20](example)). A request will return a JSON object with schema:
+
+```json
+{
+    addends: [integer, integer],
+    sol: integer,
+    wrong: [integer, integer, integer]
+}
+```
+
 ## Build & Run ##
 
 ```sh
--- src/main/scala/com/sumdog/addition/AdditionServlet.scala
+++ src/main/scala/com/sumdog/addition/AdditionServlet.scala
@@ -1,12 +1,12 @@
 package com.sumdog.addition
 
+import scala.util.Random
 import org.scalatra._
 // JSON-related libraries
 import org.json4s.{DefaultFormats, Formats}
-//
+import org.json4s.JsonAST._
 // JSON handling support from Scalatra
 import org.scalatra.json._
-
 
 class AdditionServlet extends ScalatraServlet with JacksonJsonSupport {
   // Before every action runs, set the content type to be in JSON format.
@@ -23,13 +23,21 @@ class AdditionServlet extends ScalatraServlet with JacksonJsonSupport {
       try {
         val min = minParam.get.toInt
         val max = maxParam.get.toInt
-        Error("Query parameters are " + min + " and " + max)
+        assert(min < max)
+        assert(min >= 0)
+        assert(max <= 1000000)
+        val diff = max - min
+        val addends = Seq(min + Random.nextInt(diff), min + Random.nextInt(diff))
+        val sol = addends(0) + addends(1)
+        val incorrect = Seq.fill(3)(Random.nextInt(2*max))
+        Addition(addends, sol, incorrect)
       } catch {
-        case e : Exception =>
+        case e : Throwable  =>
           Error("Please include min and max url parameters between 0 and 1,000,000")
       }
   }
   // Sets up automatic case class to JSON output serialization, required by the JValueResult trait
   protected implicit lazy val jsonFormats: Formats = DefaultFormats
 }
+case class Addition(addends: Seq[Int], sol: Int, incorrect: Seq[Int])
 case class Error(message: String)
